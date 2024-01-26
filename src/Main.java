327d6ca9233681dea05659db2179019d19298105
Mauricio <stb539@gmail.com>
2024-01-26 13:44:44 -0300

    saludo_Java

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // declarar vars
+        String nombre, apellido, apellido_dos;
+        float altura;
+        int edad, an_Actual = 2024;
+        Scanner leer = new Scanner(System.in); // instanciar la clase Scanner
+
+        String saludo = "Hola Mundo!!";
+        System.out.println(saludo);
+
+        System.out.println("Introduzca nombre:");
+        nombre = leer.nextLine();
+        System.out.println("Introduzca apellido:");
+        apellido = leer.nextLine();
+        System.out.println("Introduzca segundo apellido:");
+        apellido_dos = leer.nextLine();
+
+        System.out.println("Introduzca su altura:");
+        altura = leer.nextFloat();
+
+        System.out.println("Introduzca su edad:");
+        edad = leer.nextInt();
+
+        System.out.println("Bienvenido: " + nombre + ' ' + apellido + ' ' + apellido_dos);
+        System.out.println("de altura: " + altura);
+        System.out.println("y nacido en: " +(an_Actual-edad));
+
+        // sout //System.out.println();
+
+        /*
+        int [] arr = {45, 32,26, 14};
+
+        do {
+            System.out.println(edad);
+
+            edad = edad-1;
+            System.out.println(arr[edad]);
+            arr[edad] = edad - 1;
+        } while (edad > 0);
+        //System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+
+         */
+
+
+    }
+
+}
